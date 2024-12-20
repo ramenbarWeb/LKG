3fb57b01a8a4a2a49842c075d13aa94a7b2b95da
rags <108086246+ramenbarWeb@users.noreply.github.com>
2024-12-20 12:29:59 +0530

    Delete Assignments/CC directory

--- Assignments/CC/Assignment1/11_interest.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    float p, r, n, interest;
-    printf("Enter principal, rate, and time: ");
-    scanf("%f %f %f", &p, &r, &n);
-    interest = (p * r * n) / 100;
-    printf("Simple Interest: %.2f\n", interest);
-    return 0;
-}
